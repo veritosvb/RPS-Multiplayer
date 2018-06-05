0bb9d63179e6736a857eda82c614d07c6429a83f
Veritosvb <vero.velez@gmail.com>
2018-06-05 15:23:34 -0500

    Many bugs left

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,47 +1,199 @@
   // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyApXnD6I0oRZwcnKSbnZbTl1Ubk6O6EJ-o",
-    authDomain: "rockpaperscissors-4a31b.firebaseapp.com",
-    databaseURL: "https://rockpaperscissors-4a31b.firebaseio.com",
-    projectId: "rockpaperscissors-4a31b",
-    storageBucket: "",
-    messagingSenderId: "901060609949"
-  };
-  firebase.initializeApp(config);
-
-  var database = firebase.database();
-  var chat = database.ref("/chat");
-
-  chat.remove();
-  var game = database.ref("/games");
-  var players = {
-    creator: { displayName: "Player 1", uid: "whatever" },
-    joiner: { displayName: "Player 2", uid: "whatever2" },
-    state: 1 //open
+var config = {
+apiKey: "AIzaSyApXnD6I0oRZwcnKSbnZbTl1Ubk6O6EJ-o",
+authDomain: "rockpaperscissors-4a31b.firebaseapp.com",
+databaseURL: "https://rockpaperscissors-4a31b.firebaseio.com",
+projectId: "rockpaperscissors-4a31b",
+storageBucket: "",
+messagingSenderId: "901060609949"
 };
- var displayN = "Player 1";
-  var state = {
-      open:1, 
-      joined:2
+firebase.initializeApp(config);
+var database = firebase.database();
+
+/*
+* All reference to the childs in the database
+*
+*/
+var chat = firebase.database().ref("chat");
+var player = firebase.database().ref("/player");
+var turn = firebase.database().ref("/turn");
+var startgame = false;
+chat.remove();
+turn.remove();
+
+var userRef; // will be used  to reference the object where we will store this player info
+/*
+* Local copy of the variables in firebase
+*/
+var wins1, wins2, losses1, losses2, choise1, choise2;
+var displayName; //local player name
+var otherName;// remote player name
+var userNumber;// my player name
+
+//listener that a game is going on. Whenever we start a game turn goes to 1 and we add the players game
+ database.ref().on("value", function(snapshot) {
+    var turnVal = snapshot.child('turn').val();
+    if (turnVal !== null && player == undefined) {
+        console.log('Please wait until other players finish, then refresh screen.');
+    }
+    });
+
+
+// Listen for the increment in turn. The increment is done after selecting rps
+//In this function you will classify them 1 for player 1, 2 for 2 and 3 for printing results
+turn.on('value', function(snapshot) {
+
+    var turnNum = snapshot.val();
+    if (turnNum	== 1) {
+        $("#gameMessages").text("Player 1 turn");
+
+    } else if (turnNum == 2) {
+        $("#gameMessages").text("Player 2 turn");
+
+    } else if (turnNum == 3){
+        $(".game").removeClass("choise"); 
+        setRPS();
+        
+    }
+});
+/*
+* Inicialize all the variables
+*/
+  function setRPS() {
+    console.log("getting winner");
+    firebase.database().ref("player").once('value', function(snapshot) {
+       var ch1 = snapshot.val()[1].choice;
+       var wins1 = snapshot.val()[1].wins;
+       var losses1 = snapshot.val()[1].losses;
+       var ch2 = snapshot.val()[2].choice;
+       var wins2 = snapshot.val()[2].wins;
+       var losses2 = snapshot.val()[2].losses;
+
+       console.log("entering the game with " +ch1 + " " + ch2);
+
+        var result = rps(ch1,ch2);
+        console.log(result);
+        
+        if(result == "win"){
+            $("#gameMessages").text(ch1 + " beats " + ch2 + " "+snapshot.val()[1].name + " you won");
+            wins1++;
+            losses2++;
+            
+        }if(result == "lose"){
+            $("#gameMessages").text(ch2 + " beats " + ch1 + " "+snapshot.val()[2].name + " you won");
+            wins2++;
+            losses1++;
+            
+        }else if(result == "draw"){
+            $("#gameMessages").text(ch1 + " same " + ch2 + " "+"Draw")
+        }
+        $("#tdWinsp1").text(wins1);
+        $("#tdWinsp2").text(wins2);
+        $("#tdLossesp2").text(losses2);
+        $("#tdLossesp1").text(losses1);
+        $("#join_text").text("Try again");
+
+    });
+
     }
 
+    player.on('child_removed', function(snapshot) {
+        // Find player that was removed
+        var key = snapshot.key;
+        $("#join_text").text("Join Game");
+        // Show 'player has disconnected' on chat
+        $("gameMessages").text("Player" + key + "has left");
+        startgame = false;
+        // Empty turn message
+        $("gameMessages").text('Waiting for another player to join...');
+        // Display beginning message
+        // Empty score
+        $('#tdWinsp1').text("");
+        $('#tdWinsp2').text("");
+        $('#tdLossesp1').text("");
+        $('#tdLossesp2').text("");
+    });
 
-//create account
+//once only license once for data
+ function setPlayer() {
+    console.log("Defining if Player 1 or Player 2");
+    database.ref().once('value', function(snapshot) {
+        var playerObj = snapshot.child('player');
+        var num = playerObj.numChildren();
+        // Add player 1
+      if (num == 0) {
+          player = 1;
+          addPlayer(player);
+          $("#gameMessages").text("Wait for another player to join...");
+      } else if (num == 1 && playerObj.val()[2] !== undefined) {
+            // Sets player to 1
+          player = 1;
+         
+          addPlayer(player);
+          // Start turn by setting turn to 1
+          turn.set(1);
+      // Add player 2
+      } else if (num == 1) {
+            // Sets player to 2
+          player = 2;
+          addPlayer(player);
+          // Start turn by setting turn to 1
+            turn.set(1);
+      }
+    });
+}
+
+/*
+* Activate all buttons and add player to firebase
+*/
+function addPlayer(count) {
+    var ocount;
+    console.log( 'You are Player ' + count);
+    $("#player"+count).text(displayName);
+    // Create new child with player number
+        if(count ==1)
+            ocount =2;
+            else
+            ocount =1;
+
+    $("#rock"+ocount).hide("slow");
+    $("#paper"+ocount).hide("slow");
+    $("#scissors"+ocount).hide("slow");
+    if(displayName =="Player")
+        displayName = "Player "+ count;
+    userNumber = count;
+    otherName = ocount;
+    userRef = firebase.database().ref("player/"+count);
+    // Allows for disconnect
+    userRef.onDisconnect().remove();
+    // Sets children of player number
+    userRef.set({
+        'name': displayName,
+        'wins': 0,
+        'losses': 0
+    });
+}
+
+
+/*
+* Starts auth methods
+*/
+
+///create account
 function submitCreateAccount(){
-    var displayName = document.querySelector("#entry-displayname");
+    displayName = document.querySelector("#entry-displayname");
     var email = document.querySelector("#entry-email");
     var password = document.querySelector("#entry-password");
     firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
     .then(function(user){
     user.updateProfile({
-        displayName: displayName.value});   
+        displayName: displayName.value
+    });
+        
     }).catch(function(error) {
         $("#error").text(error.message);
          $('#errorh').modal('show'); 
         console.log(error.message);
-
-
-
     });       
 }
  
@@ -55,11 +207,9 @@ firebase.auth().signOut().then(function() {
 
 
 //sign in account
-function signIn(){
-    
+function signIn(){ 
     var email = $("#email").val();
     var password = $("#password").val();
-
     firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
@@ -71,76 +221,41 @@ function signIn(){
     });
 }
 
-
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    name = user.displayName;
-    email = user.email;
-    uid = user.uid;  
-    $("#uid").text("Sign in as: " + email);
-    displayN = firebase.auth().currentUser.displayName;
-
-  }
-  else{
-      displayN = "Player 1";
-  }
-});
-
-
-//Chat
-function sendChatMessage(){
-    var messageField = $("#chatmessage").val();
-    $("#chatmessage").val("");
-    chat.push().set({
-        name: displayN,
-        message: messageField
+    firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        email = user.email;
+        name = user.name;
+        $("#uid").text("Sign in as: " + user.displayName);
+        displayName = user.displayName;
+       // line that connects auth with game start
+    }
+    else{
+        displayName = "Player";
+    }
     });
-}
+
+    //Chat
+    function sendChatMessage(){
+        var messageField = $("#chatmessage").val();
+        $("#chatmessage").val("");
+        chat.push().set({
+            name: displayName,
+            message: messageField
+        });
+    }
 
 
-firebase.database().ref("/chat").on("child_added", function(snapshot){
-    
-    var message = snapshot.val();
-    console.log(message.message);
-    addChatMessage(message.name, message.message);
-})    
+    firebase.database().ref("chat").on("child_added", function(snapshot){
+        var message = snapshot.val();
+        console.log(message.message);
+        addChatMessage(message.name, message.message);
+    })    
 
 
-function addChatMessage(name, message){
-    $("#chat").append( "<p><strong>" + name + ":</strong> " + message + "</p>");
-}
+    function addChatMessage(name, message){
+        $("#chat").append( "<p><strong>" + name + ":</strong> " + message + "</p>");
+    }
 
-//match
-
-function createGame(){
-    var user = firebase.auth().currentUser;
-    players.creator.uid =  user.uid;
-    players.creator.displayName = user.displayName;
-    players.state = state.open
-    game.push().set(players);
-}
-
-function joinGame(key){
-    var user = firebase.auth().currentUser;
-    //just works in one single game
-    var gameRef = game.child(key);
-
-    //just 1 at the time its allowed in the transaction
-    gameRef.transaction(function(current) {
-        if(!current.joiner){
-            current.state = state.joined;
-            current.joiner = {
-                uid: user.uid,
-                displayName: user.displayName
-            }
-        return current;
-        }    
-    })
-}
-
-
-$(document).ready(function()
-{
     $("#click-register").on('click', function() { 
         $('#register').modal('show'); 
     });
@@ -176,7 +291,38 @@ $(document).ready(function()
             sendChatMessage();
         }
     }); 
-});
+
+    $('#join').on("click", function () {
+        //you can join this app after auth or with default name... later I could change it
+        console.log("Joining Game");
+        setPlayer();
+        
+    });
+    $('.game').on("click", function () {
+        var q = $(this).attr("data");
+        console.log("this is the selection from the button: " + q);
+
+
+        var turnNum;
+
+        turn.once('value', function(snapshot) {
+            turnNum = snapshot.val();
+        });
+
+        if(turnNum == userNumber){
+            console.log("Player " + turnNum + " turn");
+            $(this).addClass("choise");
+            userRef.update({
+                choice: q
+            }); 
+				turnNum++;
+				turn.set(turnNum);
+        }
+
+
+
+    });  
+
 
 
 function cleanModal(){
@@ -189,3 +335,41 @@ function cleanModal(){
     $('#signIn').modal('hide'); 
     $('#errorh').modal('hide'); 
 }
+
+function rps(yourRPS, opponentRPS) {
+    // Run traditional rock, paper, scissors logic and return whether you won, lost, or had a draw.
+        switch(yourRPS) {
+        case 'rock':
+          switch(opponentRPS) {
+                case 'rock':
+                    return 'draw';
+                case 'paper':
+                    return 'lose';
+                case 'scissors':
+                    return 'win';
+            }
+          break;
+        case 'paper':
+            switch(opponentRPS) {
+                case 'rock':
+                    return 'win';
+                case 'paper':
+                    return 'draw';
+                case 'scissors':
+                    return 'lose';
+            }
+          break;
+        case 'scissors':
+            switch(opponentRPS) {
+                case 'rock':
+                    return 'lose';
+                case 'paper':
+                    return 'win';
+                case 'scissors':
+                    return 'draw';
+            }
+            break;
+        }
+     }
+    // _Called after player assignment completes._
+    
